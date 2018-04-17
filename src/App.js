86ff73e3bc5a0d97095051be273dd4e5d84d4017
rajatk-31 <rajatky07@gmail.com>
2018-04-17 23:36:31 +0530

    starting eject now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import Radium, {StyleRoot} from 'radium';
 import './App.css';
 import Person from './person/person'
 
@@ -9,10 +9,11 @@ class App extends Component {
    */
   state = {  ///this should be a javascript object.
     persons: [
-      {name: "Rajat", age: 22},
-      {name: "Naveen", age: 23},
-      {name: "Omprakash", age: 21}
-    ]
+      {key:"rajat1", name: "Rajat", age: 22},
+      {key:"rajat2", name: "Naveen", age: 23},
+      {key:"rajat3", name: "Omprakash", age: 21}
+    ],
+    showPerson: false
   }
 
   /**
@@ -21,9 +22,9 @@ class App extends Component {
   switchNameHandler = (newName)=> {
     this.setState({
       persons: [
-        {name: "Rajat", age: 25},
-        {name: newName, age: 23},
-        {name: "Omprakash", age: 21}
+        { name: "Rajat", age: 25},
+        { name: newName, age: 23},
+        { name: "Omprakash", age: 21}
       ]
     })
   }
@@ -31,43 +32,108 @@ class App extends Component {
   /**
    * Name  Change Handler
    */
-  nameChangeHandler = event=>{
+  nameChangeHandler = (event,key)=>{
+    const personIndex=this.state.persons.findIndex(p=>{
+      return p.key===key;
+    })
+    const person = {...this.state.persons[personIndex]}
+    person.name = event.target.value
+    const persons = [...this.state.persons]
+    persons[personIndex] = person;
     this.setState({
-      persons: [
-        {name: "Rajat", age: 25},
-        {name: event.target.value, age: 23},
-        {name: "Omprakash", age: 21}
-      ]
+      persons: persons
+    })
+    // this.setState({
+    //   persons: [
+    //     { name: "Rajat", age: 25},
+    //     { name: event.target.value, age: 23},
+    //     { name: "Omprakash", age: 21}
+    //   ]
+    // })
+  }
+
+  /**
+   * Toggle Handler
+   */
+  toggleHandler = ()=>{
+    this.setState({
+      showPerson: !this.state.showPerson
     })
   }
+
+    /**
+     * Delete Person
+     */
+    deletePerson = index => {
+      // const per = this.state.persons;
+      const per= [...this.state.persons]
+      per.splice(index,1);
+      this.setState({persons: per})
+    }
 
   render() {
     /**
      * Inline styles
      */
     const style = {
-      backgroundColor: "Red",
+      backgroundColor: "white",
       font: "inherit",
       padding: '8px',
-      border: "1px solid green",
-      cursor: 'pointer'
+      border: "1px solid red",
+      cursor: 'pointer',
+      // ':hover': {
+      //   backgroundColor: 'black',
+      //   color: 'white'
+      // }
+    }
+    let personss= null;
+    if(this.state.showPerson){
+      personss= (
+        <div>
+          {this.state.persons.map((person,index)=>{
+            return <Person 
+              name={person.name}
+              age={person.age}
+              key ={person.key}
+              changed = {(event)=>this.nameChangeHandler(event,person.key)}
+              click ={()=>this.deletePerson(index)
+            }></Person>
+          })},
+          {/* <Person name= {this.state.persons[0].name} age={this.state.persons[0].age}/>
+        
+          <Person name= {
+            this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click ={this.switchNameHandler.bind(this, "Ramu!")}
+            change ={this.nameChangeHandler}>Hobbies: Quiz </Person>
+          <Person name= {this.state.persons[2].name} age={this.state.persons[2].age}/> */}
+        </div>
+      )
+      style.backgroundColor = 'red'
+    }
+    const classes = [];
+    if(this.state.persons.length<=3){
+      classes.push('Red')
+    }
+    if(this.state.persons.length>=2){
+      classes.push('Bold')
+    }
+    if(this.state.showPerson){
+      classes.push('Green')
     }
     return (
-      <div className="App">
-        <h1>Hi, this is rajat starting the react again</h1>
-        <p>Something is not good</p>
-        {/* One way of calling function.
-          This can be inefficient at times . So try to ignore this syntax */}
-        <button style={style} onClick ={()=> this.switchNameHandler("Amit")}>Switch Names</button>
-        <Person name= {this.state.persons[0].name} age={this.state.persons[0].age}/>
-        {/* Other way of calling function */}
-        <Person name= {
-          this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click ={this.switchNameHandler.bind(this, "Ramu!")}
-          change ={this.nameChangeHandler}>Hobbies: Quiz </Person>
-        <Person name= {this.state.persons[2].name} age={this.state.persons[2].age}/>
-      </div>
+      
+        <div className="App">
+          <h1>Hi, this is rajat starting the react again</h1>
+          <p className = {classes.join(' ')}>Something is not good</p>
+          {/* One way of calling function.
+            This can be inefficient at times . So try to ignore this syntax */}
+          {/* <button style={style} onClick ={()=> this.switchNameHandler("Amit")}>Switch Names</button> */}
+          <button style={style} onClick ={this.toggleHandler}>Switch Names</button>
+          {personss}
+        </div>
+      
+      
     );
   }
 }
--- src/person/person.js
+++ src/person/person.js
@@ -1,12 +1,18 @@
 import React from "react";
+// import Radium from 'radium';
 import './person.css'
 
 const person = (props)=>{
+    // const styles ={
+    //     '@media (min-width:500px)': {
+    //         width: '450px'
+    //     }
+    // }
     return (
         <div className = "Person">
-            <p>Hi this is {props.name} and i am {props.age} years old.</p>
-            <p onClick = {props.click}>{props.children}</p>
-            <input type= "text" onChange={props.change} value={props.name} />
+            <p onClick={props.click}>Hi this is {props.name} and i am {props.age} years old.</p>
+            {/* <p onClick = {props.click}>{props.children}</p> */}
+            <input type= "text" onChange={props.changed} value={props.name} />
         </div>
     )
 }
